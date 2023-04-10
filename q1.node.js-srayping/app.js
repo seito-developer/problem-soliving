67fcf3b83f2c558badf0295e91f843aa50b3980a
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-04-10 17:56:39 +0900

    OK q1

---
+++ q1.node.js-srayping/app.js
@@ -0,0 +1,54 @@
+const rp = require('request-promise');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const jsonfile = require('jsonfile');
+
+const CONFIG = require('./config.js');
+
+let pageCount = 1;
+let articleArray = [];
+
+const updateOutput = (data) => {
+	jsonfile.writeFile(CONFIG.OUTPUT, data, (err) => {
+	  if(err) {
+	    console.log(err);
+	  } else {
+	    console.log('Created a file');
+	  }
+	});
+};
+
+const getData = (htmlString) => {
+	const htmlDom = new JSDOM(htmlString);
+	const $title = htmlDom.window.document.getElementsByClassName(CONFIG.HEADING_CLASS);
+	const articleLen = $title.length;
+
+ 	let index = 0;
+
+  	while(index < articleLen){
+  		let $target = $title[index].getElementsByTagName('a')[0];
+  		let obj = {
+  			title: $target.textContent,
+  			url: $target.getAttribute('href')
+  		};
+
+		if(obj.title.indexOf(CONFIG.KEYWORD) !== -1){
+			articleArray.push(obj);
+		}
+  		index++;
+};
+
+const createList = (count) => {
+	rp.get(CONFIG.TARGET_URL + pageCount)
+		.then((htmlString) => {
+			getData(htmlString);
+			if(pageCount < CONFIG.PAGES_LEN){
+				pageCount++;
+				createList(pageCount);
+			} else {
+				updateOutput(articleArray);
+				console.log('articleArray', articleArray);
+			}
+		});
+};
+createList(pageCount);
